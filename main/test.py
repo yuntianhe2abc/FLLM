da5fb140c3416cdbf2b47c0321726337a22bdecc
Yansong Li <liyansong2abc@gmail.com>
2023-04-12 12:00:17 +1000

    experiment(client1,PPL-XL_Zlib top 1000, de-duplicated)

--- main/test.py
+++ main/test.py
@@ -1,12 +1,11 @@
-# import pickle
-# with open("/Users/macm2/PycharmProjects/FLLM_black_box/result/score_dictionaries/clients_1_3_PPL-XL_Zlib.txt", 'rb') as fp:
-#     top_scores=pickle.load(fp)
-#     print(top_scores)
-# file = open("Fruits.obj",'rb')
-# object_file = pickle.load(file)
-# file.close()
-a={}
-b=a.get(2,[])
-print(b)
-a[2].extend([2,43,56])
-print(b)
+import pickle
+with open("../result/score_dictionaries/clients_1_1_PPL-XL_Zlib.pkl", 'rb') as fp:
+    top_scores=pickle.load(fp)
+    print(top_scores)
+fp.close()
+
+# a={}
+# b=a.get(2,[])
+# print(b)
+# a[2].extend([2,43,56])
+# print(b)
